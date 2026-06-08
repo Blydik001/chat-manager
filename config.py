02a62e266dc661954c9bc9286c069665dffb4fdd
Blydik001 <blydik35@gmail.com>
2026-06-09 00:39:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8975105830:AAHSsz6kETM1gD-x2JnOlfVTeLOWMRp_8I4")
+OWNER_IDS = list(map(int, os.getenv("8881305868", "").split(','))) if os.getenv("OWNER_IDS") else []
